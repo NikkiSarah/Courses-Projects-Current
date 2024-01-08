0173e0ebf8ce45e434026862763c71285af50ce6
NikkiSarah <81358254+NikkiSarah@users.noreply.github.com>
2024-01-08 22:42:10 +1100

    Update chapters

--- 2024 Hands-On Machine Learning/Chapter 8.py
+++ 2024 Hands-On Machine Learning/Chapter 8.py
@@ -1,8 +1,127 @@
 # train a random forest classifier, timing how long it takes, and evaluate on the test set
-
-# use PCA to reduce the dimensionality with an explained variance of 95% and repeat
-
-# use t-sne to reduce the dimensionality to 2 dimensions and plot the result
+import numpy as np
+from sklearn.datasets import fetch_openml
+import os
+import time
 import matplotlib.pyplot as plt
 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA, LatentDirichletAllocation, NMF, TruncatedSVD
+from sklearn.manifold import *
+
+# check the backend and change if required
+import matplotlib as mpl
+
+mpl_backend = mpl.get_backend()
+if mpl_backend != "Qt5Agg":
+    mpl.use("Qt5Agg")
+else:
+    pass
+
+n_cpu = os.cpu_count()
+print("Number of CPUs in the system:", n_cpu)
+
+mnist = fetch_openml('mnist_784', version=1, parser='auto')
+X, y = mnist.data, mnist.target
+
+X = X / 255.0
+y = y.astype(np.uint8)
+
+X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, stratify=y, train_size=60000)
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, stratify=y_train_val, train_size=50000)
+
+t0 = time.time()
+clf = RandomForestClassifier(random_state=42)
+clf.fit(X_train, y_train)
+print(time.time() - t0)
+print(clf.score(X_test, y_test))
+# the model with the raw data takes about 53.2 seconds to train and has a test set accuracy of 97.19
+
+# use PCA to reduce the dimensionality with an explained variance of 95% and repeat
+pca = PCA(n_components=0.95)
+X_train_reduced = pca.fit_transform(X_train)
+
+t0 = time.time()
+clf = RandomForestClassifier(random_state=42)
+clf.fit(X_train_reduced, y_train)
+print(time.time() - t0)
+
+X_test_reduced = pca.transform(X_test)
+print(clf.score(X_test_reduced, y_test))
+# the model with the reduced data takes about 215 seconds (much slower) to train and has a test set accuracy of 95.06
+
+# use t-sne to reduce the dimensionality to 2 dimensions and plot the result
+# sub-sample 10,000 images to reduce computation time
+X, X_test, y, y_test = train_test_split(X_train_val, y_train_val, train_size=10000, stratify=y_train_val)
+
+t0 = time.time()
+tsne = TSNE(n_components=2, n_jobs=n_cpu-2, random_state=42)
+X_reduced = tsne.fit_transform(X)
+print(time.time() - t0)
+
+
+def visualise_reduction(title, X_reduced=X_reduced, y=y):
+    plt.scatter(X_reduced[:, 0], X_reduced[:, 1], c=y, marker=".")
+    for i in range(len(np.unique(y))):
+        indices = (y == i)
+        center_x = np.mean(X_reduced[indices, 0])
+        center_y = np.mean(X_reduced[indices, 1])
+        plt.text(center_x, center_y, str(i), fontsize=20, ha='center', va='center', color='red', weight='bold')
+    plt.title(title, weight='bold', fontsize=25)
+    plt.axis('off')
+
+
+visualise_reduction("T-Distributed Stochastic Neighbour Embedding (T-SNE)")
+
 # repeat with other techniques like PCA, LLE and MDS
+t0 = time.time()
+isomap = Isomap(n_components=2, n_jobs=n_cpu-2)
+X_reduced = isomap.fit_transform(X)
+print(time.time() - t0)
+visualise_reduction("Isometric Mapping")
+
+t0 = time.time()
+lle = LocallyLinearEmbedding(n_components=2, n_jobs=n_cpu-2, random_state=42)
+X_reduced = lle.fit_transform(X)
+print(time.time() - t0)
+visualise_reduction("Locally Linear Embedding")
+
+t0 = time.time()
+spectral = SpectralEmbedding(n_components=2, n_jobs=n_cpu-2, random_state=42)
+X_reduced = spectral.fit_transform(X)
+print(time.time() - t0)
+visualise_reduction("Spectral Embedding")
+
+# sub-sample 10,000 images to reduce computation time
+X_mds, X_test, y_mds, y_test = train_test_split(X_train_val, y_train_val, train_size=2000, stratify=y_train_val)
+
+t0 = time.time()
+mds = MDS(n_components=2, n_jobs=n_cpu-2, random_state=42, normalized_stress='auto')
+X_reduced = mds.fit_transform(X_mds)
+print(time.time() - t0)
+visualise_reduction("Multidimensional Scaling", X_reduced, y_mds)
+
+t0 = time.time()
+pca = PCA(n_components=2, random_state=42)
+X_reduced = pca.fit_transform(X)
+print(time.time() - t0)
+visualise_reduction("Principal Component Analysis")
+
+t0 = time.time()
+lda = LatentDirichletAllocation(n_components=2, n_jobs=n_cpu-2, random_state=42)
+X_reduced = lda.fit_transform(X)
+print(time.time() - t0)
+visualise_reduction("Latent Dirichlet Allocation")
+
+t0 = time.time()
+nmf = NMF(n_components=2, random_state=42)
+X_reduced = nmf.fit_transform(X)
+print(time.time() - t0)
+visualise_reduction("Non-Negative Matrix Factorisation")
+
+t0 = time.time()
+svd = TruncatedSVD(n_components=2, random_state=42)
+X_reduced = svd.fit_transform(X)
+print(time.time() - t0)
+visualise_reduction("Truncated Singular Value Decomposition aka Latent Semantic Analysis")
--- 2024 Hands-On Machine Learning/Chapter 9.py
+++ 2024 Hands-On Machine Learning/Chapter 9.py
@@ -1,19 +1,142 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.datasets
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.decomposition import PCA
+from sklearn.mixture import GaussianMixture
+
+# check the backend and change if required
+import matplotlib as mpl
+
+mpl_backend = mpl.get_backend()
+if mpl_backend != "Qt5Agg":
+    mpl.use("Qt5Agg")
+else:
+    pass
+
+n_cpu = os.cpu_count()
+print("Number of CPUs in the system:", n_cpu)
+
 # load the olivetti faces dataset, split it into training, validation and test sets using stratified sampling
+data = sklearn.datasets.fetch_olivetti_faces(data_home='./datasets', return_X_y=True)
+X, y = data[0], data[1]
+
+X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, stratify=y, test_size=0.15)
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, stratify=y_train_val, test_size=0.15)
 
 # cluster the images with k-means
+metrics = []
+for k in range(5, 340, 5):
+    print('Num clusters:', k)
+    kmeans = KMeans(n_clusters=k, n_init='auto')
+    kmeans.fit(X_train_val)
+    metrics.append((k, kmeans.inertia_, silhouette_score(X_train_val, kmeans.labels_), kmeans))
+metrics_df = pd.DataFrame(metrics, columns=['k', 'inertia', 'silhouette_score', 'model'])
+
+plt.scatter(metrics_df.k, metrics_df.inertia)
+plt.title("Inertia as a function of the number of clusters")
+
+plt.scatter(metrics_df.k, metrics_df.silhouette_score)
+plt.title("Silhouette scores as a function of the number of clusters")
+# the best number of clusters is ~142 according to the silhouette scores; inertia isn't particularly informative
+
+best_model = metrics_df.iloc[np.argmax(metrics_df.silhouette_score)]['model']
+
+
+# visualise the clusters
+def plot_faces(faces, labels, n_cols=5):
+    faces = faces.reshape(-1, 64, 64)
+    n_rows = (len(faces) - 1) // n_cols + 1
+    plt.figure(figsize=(n_cols, n_rows * 1.1))
+    for idx, (face, label) in enumerate(zip(faces, labels)):
+        plt.subplot(n_rows, n_cols, idx+1)
+        plt.imshow(face, cmap="grey")
+        plt.axis("off")
+        plt.title(label)
+
+num_clusters = 10
+for cluster_id in np.unique(best_model.labels_):
+    if cluster_id <= num_clusters:
+        print("Cluster", cluster_id)
+        in_cluster = best_model.labels_ == cluster_id
+        faces = X_train_val[in_cluster]
+        labels = y_train_val[in_cluster]
+        plot_faces(faces, labels)
+    else:
+        pass
 
 # train a classifier to predict which person is represented in each image and evaluate on the validation set
+et_clf = ExtraTreesClassifier(n_jobs=n_cpu-2, random_state=42)
+et_clf.fit(X_train, y_train)
+print(et_clf.score(X_val, y_val))  # 0.9804
 
 # use k-means as a dimensionality reduction tool and re-train the classifier on the reduced dataset
+X_train_reduced = best_model.transform(X_train)
+X_val_reduced = best_model.transform(X_val)
 
-# train a third classifier on a dataset where the reduced features are appended to the original data
+et_clf = ExtraTreesClassifier(n_jobs=n_cpu-2, random_state=42)
+et_clf.fit(X_train_reduced, y_train)
+print(et_clf.score(X_val_reduced, y_val))  # 0.8824
+
+# find the number of clusters that gives the best performance
+scores = []
+for k in range(5, 290, 5):
+    print('Num clusters:', k)
+    kmeans = KMeans(n_clusters=k, n_init='auto')
+    X_train_reduced = kmeans.fit_transform(X_train)
+    clf = ExtraTreesClassifier(random_state=42, n_jobs=n_cpu-2)
+    clf.fit(X_train_reduced, y_train)
+    X_val_reduced = kmeans.transform(X_val)
+    scores.append((k, clf.score(X_val_reduced, y_val)))
+scores_df = pd.DataFrame(scores, columns=['k', 'accuracy'])
+
+plt.scatter(scores_df.k, scores_df.accuracy)
+plt.title("Validation set accuracy as a function of k")
+best_k = scores_df.iloc[np.argmax(scores_df.accuracy)]['k']
+# we can get an accuracy of about 0.9219 with 130 clusters
+
+# repeat but with the reduced features appended to the original dataset
+scores = []
+for k in range(5, 290, 5):
+    print('Num clusters:', k)
+    kmeans = KMeans(n_clusters=k, n_init='auto')
+    X_train_reduced = kmeans.fit_transform(X_train)
+    X_train_extended = np.c_[X_train, X_train_reduced]
+
+    clf = ExtraTreesClassifier(random_state=42, n_jobs=n_cpu-2)
+    clf.fit(X_train_extended, y_train)
+    X_val_reduced = kmeans.transform(X_val)
+    X_val_extended = np.c_[X_val, X_val_reduced]
+    scores.append((k, clf.score(X_val_extended, y_val)))
+scores_df = pd.DataFrame(scores, columns=['k', 'accuracy'])
+
+plt.scatter(scores_df.k, scores_df.accuracy)
+plt.title("Validation set accuracy as a function of k")
+best_k = scores_df.iloc[np.argmax(scores_df.accuracy)]['k']
+# we can get an accuracy similar to the original classifier with just 5 clusters
 
 # train a Gaussian mixture model and use the model to generate and visualise new faces
+pca = PCA(n_components=0.99)
+X_train_reduced = pca.fit_transform(X_train)
+X_val_reduced = pca.transform(X_val)
+
+gmm = GaussianMixture(n_components=40, random_state=42, verbose=1)
+gmm.fit(X_train, y_train)
+y_pred = gmm.predict(X_val_reduced)
+
+num_new = 20
+new_faces_reduced, new_labels = gmm.sample(n_samples=num_new)
+new_faces = pca.inverse_transform(new_faces_reduced)
+plot_faces(new_faces)
 
 # modify some images and see if the model can detect the anomalies
 
-# use PCA to perform dimensionality reduction, preserving 99% of the variance
-
 # compute the reconstruction error for each image
+
 
 # compute the reconstruction error for the modified images and plot one (of the reconstructed images)
