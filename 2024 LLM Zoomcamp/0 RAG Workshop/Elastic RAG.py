1eb25bde0b83d15e207bf0762383a9dbbcb6aa7f
NikkiSarah <81358254+NikkiSarah@users.noreply.github.com>
2024-07-13 09:12:46 +0000

    Add codespace edits

--- 2024 LLM Zoomcamp/0 RAG Workshop/Elastic RAG.py
+++ 2024 LLM Zoomcamp/0 RAG Workshop/Elastic RAG.py
@@ -1,7 +1,12 @@
-#%% Load the data
+#%% Load and Prepare Data
+import os
 import json
 
-with open ('./0 RAG Workshop/Zoomcamp FAQs.json', 'rt') as f:
+# check the current working directory
+os.getcwd()
+
+# unnest/flatten the file structure
+with open ('documents.json', 'rt') as f:
     data_file = json.load(f)
 
 documents = []
@@ -15,22 +20,12 @@ for course in data_file:
 print(documents[2])
 print(f"There are: {len(documents)} documents.")
         
-#%% Index data
-import os
+# %% Index Data with elasticsearch
+# nb: indexing is like creating a table in a relational db
 from elasticsearch import Elasticsearch
 
-# in a CLI, change the directory and enter bin/elasticsearch.bat to open a
-# connection
-API_KEY = os.environ.get("ES_API_KEY")
-
-# initiate the connection and check it's working
-es = Elasticsearch()
-es.info()
-
-client = Elasticsearch("http://localhost:9200/", api_key="YOUR_API_KEY")
-
+# check it's working
 es = Elasticsearch("http://localhost:9200")
 es.info()
 
-
- 
+#
