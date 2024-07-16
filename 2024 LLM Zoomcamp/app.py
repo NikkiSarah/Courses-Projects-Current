5e7a820236d345ec440b50d607d291d9740e663c
NikkiSarah <81358254+NikkiSarah@users.noreply.github.com>
2024-07-16 11:21:20 +0000

    Finish course content for Wk 2

--- 2024 LLM Zoomcamp/app.py
+++ 2024 LLM Zoomcamp/app.py
@@ -1,61 +1,21 @@
 # %% load the libraries
+import streamlit as st
+import time
 from openai import OpenAI
 from elasticsearch import Elasticsearch
-import requests
-from tqdm.auto import tqdm
 
 
-# %% define the LLM client
+# %% define the clients
 client = OpenAI(
     base_url='http://localhost:11434/v1/',
     api_key='ollama'
 )
 
-
-# %% create the elastic search index
 es = Elasticsearch("http://localhost:9200")
-
-index_settings = {
-    "settings": {
-        "number_of_shards": 1,
-        "number_of_replicas": 0
-    },
-    "mappings": {
-        "properties": {
-            "text": {"type": "text"},
-            "section": {"type": "text"},
-            "question": {"type": "text"},
-            "course": {"type": "keyword"}
-        }
-    }
-}
-
-index_name = "course-questions"
-response = es.indices.create(index=index_name, body=index_settings)
-
-
-# %% load and parse the data
-docs_url = 'https://github.com/DataTalksClub/llm-zoomcamp/blob/main/01-intro/documents.json?raw=1'
-docs_response = requests.get(docs_url)
-documents_raw = docs_response.json()
-
-documents = []
-
-for course in documents_raw:
-    course_name = course['course']
-
-    for doc in course['documents']:
-        doc['course'] = course_name
-        documents.append(doc)
-
-
-# %% index the documents
-for doc in tqdm(documents):
-    es.index(index=index_name, document=doc)
 
 
 # %% define a search function
-def retrieve_documents(query):   
+def elastic_search(query, index_name = "course-questions"):   
     search_query = {
         "size": 5,
         "query": {
@@ -112,12 +72,24 @@ def llm(prompt):
 
 # %% define RAG function
 def rag(query):
-    search_results = retrieve_documents(query)
+    search_results = elastic_search(query)
     prompt = build_prompt(query, search_results)
     answer = llm(prompt)
     return answer
 
 
-# %% test the operation of the rag function
-query = "I just discovered the course. Can I still join it?"
-print(rag(query))
+# %% generate the app
+# note that this assumes the index has been created and the source documents ingested
+def main():
+    st.title("RAG Function Invocation")
+
+    user_input = st.text_input("Enter your input:")
+
+    if st.button("Ask"):
+        with st.spinner('Processing...'):
+            output = rag(user_input)
+            st.success("Completed!")
+            st.write(output)
+
+if __name__ == "__main__":
+    main()
